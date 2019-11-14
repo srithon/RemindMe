644454250a392ad654d8932f9bdaf1005d61024d
Sridaran Thoniyil <sri7thon@gmail.com>
2019-11-14 17:57:01 -0500

    Get program to run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+fn main()
+{
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+    if let Some(c) = matches.value_of("set_file")
+    {
+        // TODO handle
+        return;
+    }
 }
