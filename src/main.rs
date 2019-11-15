f6877d74d3ae47d5e458c0bde07f9fa06d0f3fd9
Sridaran Thoniyil <sri7thon@gmail.com>
2019-11-14 19:08:47 -0500

    Implemented base functionality for list

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate clap;
 use clap::App;
 use directories::UserDirs;
 
+use std::env;
+use std::fs;
+use std::io::{BufRead, BufReader};
+
 fn main()
 {
     let yaml = load_yaml!("cli.yml");
@@ -14,18 +18,18 @@ fn main()
         return;
     }
 
-    let baseDirectory;
+    let baseDirectoryName;
     if let Some(user_dirs) = UserDirs::new()
     {
-        baseDirectory = user_dirs.home_dir().join(".remindme/");
-        // println!("{}", baseDirectory.display());
+        baseDirectoryName = user_dirs.home_dir().join(".remindme/");
+        // println!("{}", baseDirectoryName.display());
     }
     else
     {
         panic!("Cannot find base directory");
     }
 
-    let todoFile = baseDirectory.join("todo");
+    let todoFileName = baseDirectoryName.join("todo");
 
     match matches.subcommand()
     {
@@ -38,6 +42,13 @@ fn main()
         // list is the default subcommand
         (_, subMatchesMaybe) => {
             println!("Used list");
+            let todoFile = fs::File::open(todoFileName).unwrap();
+            let reader = BufReader::new(todoFile);
+
+            for (index, line) in reader.lines().enumerate() {
+                let line = line.unwrap();
+                println!("{}. {}", index + 1, line);
+            }
         }
     }
 }
