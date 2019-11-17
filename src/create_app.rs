dd56bda7591ec8d96d69cc3d2a30f6561921c442
Sridaran Thoniyil <sri7thon@gmail.com>
2019-11-16 22:02:28 -0500

    Migrate cli api from yaml to code and add debug

---
+++ src/create_app.rs
@@ -0,0 +1,116 @@
+extern crate clap;
+use clap::{App, AppSettings, Arg, SubCommand};
+
+pub fn create_app_object() -> App<'static, 'static> {
+    App::new("remind-me")
+        .version("1.0")
+        .author("Sridaran Thoniyil")
+        .about("Reminds user to do things")
+        .setting(AppSettings::DontDelimitTrailingValues)
+        .setting(AppSettings::TrailingVarArg)
+        .arg(
+            Arg::with_name("group")
+                .help("Group to search for task in")
+                .short("g")
+                .long("group")
+                .takes_value(true))
+        .subcommand(
+            SubCommand::with_name("finish")
+                .about("Marks task as completed")
+                .version("1.0")
+                .author("Sridaran T.")
+                .args(
+                    &[
+                        Arg::with_name("group")
+                            .short("g")
+                            .long("group")
+                            .help("Name of the group to search for task in")
+                            .required(true)
+                            .default_value(""),
+                        Arg::with_name("INPUT")
+                            .help("Takes in index or substring of task in group to mark completed")
+                            .required(true)
+                            .min_values(1)
+                            .index(1)
+                    ]
+                ))
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("Adds task to be completed")
+                .version("1.0")
+                .author("Sridaran T.")
+                .args(
+                    &[
+                        Arg::with_name("group")
+                            .short("g")
+                            .long("group")
+                            .help("Name of the group to search for task in")
+                            .required(true)
+                            .default_value(""),
+                        Arg::with_name("INPUT")
+                            .help("Takes in string to be added to group")
+                            .required(true)
+                            .min_values(1)
+                            .index(1)
+                    ]
+                ))
+        .subcommand(
+            SubCommand::with_name("list")
+                .about("List current tasks to do")
+                .version("1.0")
+                .author("Sridaran T.")
+                .arg(
+                    Arg::with_name("group-name")
+                        .help("Name of the group to search for task in")
+                        .index(1)
+                        .takes_value(true)
+                ))
+        .subcommand(
+            SubCommand::with_name("group")
+                .about("Handles all group-related commands")
+                .version("1.0")
+                .author("Sridaran T.")
+                .subcommands(
+                    vec![
+                        SubCommand::with_name("create")
+                            .about("Creates a new group")
+                            .version("1.0")
+                            .author("Sridaran T.")
+                            .arg(
+                                Arg::with_name("group-name")
+                                    .required(true)
+                                    .index(1)
+                            ),
+                        SubCommand::with_name("delete")
+                            .about("Deletes an existing group")
+                            .version("1.0")
+                            .author("Sridaran T.")
+                            .arg(
+                                Arg::with_name("group-name")
+                                    .required(true)
+                                    .index(1)
+                            ),
+                        SubCommand::with_name("clear")
+                            .about("Name of the group to be cleared")
+                            .version("1.0")
+                            .author("Sridaran T.")
+                            .arg(
+                                Arg::with_name("group-name")
+                                    .required(true)
+                                    .index(1)
+                            )
+                    ]
+                ))
+        .subcommand(
+            SubCommand::with_name("config")
+                .about("Edit configuration of RemindMe")
+                .version("1.0")
+                .author("Sridaran T.")
+                .arg(
+                    Arg::with_name("print_configuration")
+                        .help("Prints all current configuration options")
+                        .long("print")
+                        .short("p")
+                )
+        )       
+}
--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,14 @@ use std::env;
 use std::fs::{OpenOptions, File};
 use std::io::{BufRead, BufReader, Write};
 
+mod create_app;
+
 fn main()
 {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from(yaml).get_matches();
-
-    if let Some(c) = matches.value_of("set_file")
-    {
-        // TODO handle
-        return;
-    }
-
+    // let yaml = load_yaml!("cli.yml");
+    // let matches = App::from(yaml).get_matches();
+    let matches = create_app::create_app_object().get_matches();
+    
     let baseDirectoryName;
     if let Some(user_dirs) = UserDirs::new()
     {
@@ -29,12 +26,39 @@ fn main()
         panic!("Cannot find base directory");
     }
 
-    let todoFileName = baseDirectoryName.join("todo");
+    let baseTodoFileName = "todo";
+
+    let todoFileName = baseDirectoryName.join(&baseTodoFileName);
 
     match matches.subcommand()
     {
         ("finish", subMatchesMaybe) => {
             println!("Used finish");
+
+            /*
+            finish takes in either
+                a) index of the task in the file
+                b) substring of the task
+            if index is provided, add "FINISHED"
+            if substring provided, list all tasks that match,
+                and then do same index thing
+            */
+
+            // figure out if index or substring
+            if let Some(subMatches) = subMatchesMaybe {
+                let finishGroup;
+                if let Some(groupName) = subMatches.value_of("group")
+                {
+                    finishGroup = groupName;
+                }
+                else
+                {
+                    // default value
+                    finishGroup = baseTodoFileName;
+                }
+
+                
+            }
         },
         ("add", subMatchesMaybe) => {
             println!("Used add");
@@ -42,12 +66,21 @@ fn main()
             let mut string = "".to_string();
 
             if let Some(subMatches) = subMatchesMaybe {
+                if let Some(groupName) = subMatches.value_of("group-name") {
+                    println!("group name: {}", groupName);
+                }
                 if let Some(listOfWords) = subMatches.values_of("INPUT") {
+                    if let Some(words) = subMatches.value_of("INPUT") {
+                        println!("first value: {}", words);
+                    }
+                    let mut word_count = 0;
                     listOfWords.for_each(|word| {
-                        string.push_str(&word);
-                        string.push_str(&" ".to_string())
+                            string.push_str(&word);
+                            string.push_str(&" ".to_string());
+                            word_count += 1;
                         }
                     );
+                    println!("Word count: {}", word_count);
                     // println!("Full string: \"{}\"", string);
                 }
                 else
@@ -74,6 +107,17 @@ fn main()
 
             file.write_fmt(format_args!("{}\n", string));
                 
+        },
+        ("config", subMatchesMaybe) => {
+            if let Some(subMatches) = subMatchesMaybe {
+                if let Some(sub_group) = subMatches.value_of("group") {
+                    println!("Group has a value: {}", sub_group);
+                }
+                else
+                {
+                    println!("Group does not have a value");
+                }
+            }
         },
         // list is the default subcommand
         (_, subMatchesMaybe) => {
